7dcbe05311cef23f38e3df6f47563ed2d4958210
Kim Morin <kimmorin93@gmail.com>
2017-12-07 10:48:35 -0500

    full citrusclean project

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Viewport Meta -->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/css/app.css">
+
+    <!-- Mobile Theme Colour -->
+
+    <meta name="theme-color" content="#8e2c33">
+
+    <!-- Title and Description -->
+
+    <title></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Favicons -->
+
+    <link rel="icon" href="assets/img/lemon.png">
+
+    <!-- Open Graph -->
+
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:image" content="assets/img/og.jpg">
+
+
+    <!--[if lt IE 9]>
+     <script src="js/polyfill/html5-shiv.js"></script>
+  <![endif]-->
+
+
+</head>
+<body>
+
+    <section class="landing">
+        <div class="landing__container container">
+            <div class="logo">
+                <img src="assets/img/logo.png" alt="Citrus Clean housekeeping">
+            </div>
+
+            <h1>Website Coming Soon</h1>
+
+            <div class="contact">
+                <div class="contact__container">
+                    <div class="contact-method">
+                        <img src="assets/img/lemon.png" alt="phone">
+                        <a href="tel:519.551.7844">519.551.7844</a>
+                    </div>
+                    <div class="contact-method">
+                        <img src="assets/img/orange.png" alt="email">
+                        <a href="mailto:stephanie@citrusclean.co">stephanie@citrusclean.co</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="/assets/js/app.js"></script>
+
+    <!-- Mister Sandman...sand me a man... -->
+</body>
+</html>
